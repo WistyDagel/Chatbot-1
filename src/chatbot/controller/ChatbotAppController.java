105ed45f3ee819a33a523abfad1ad846df82248b
dosmond <dosmond37@gmail.com>
2014-10-20 09:13:02 -0600

    GUI!
    Added the frame and panel GUI methods and classes.

--- src/chatbot/controller/ChatbotAppController.java
+++ src/chatbot/controller/ChatbotAppController.java
@@ -1,32 +1,44 @@
 package chatbot.controller;
 
 import chatbot.model.Chatbot;
+import chatbot.view.ChatbotFrame;
 import chatbot.view.ChatbotView;
+
 /**
  * Application Controller for the Chatbot String projoct. Controls the View and Model packages.
  * @author Daniel Osmond
  * @version 1.2 10/1/14
  */
+
 public class ChatbotAppController
 {
 	/**
 	 * The GUI for the app
 	 */
+	
 	private ChatbotView appView;
 	/**
 	 * Chatbot Model instance
 	 */
+	
 	private Chatbot notSoCleverbot;
 	/**
 	 * The Startup message.
 	 */
+	
 	private String startMessage;
+	/**
+	 * GUI Frame for the app.
+	 */
+	private ChatbotFrame baseFrame;
 /**
  * Creates a ChatbotAppController and initializes the assoiated View and Model objects.	
  */
+	
 	public ChatbotAppController()
 	{
 		appView = new ChatbotView(this);
+		baseFrame = new ChatbotFrame(this);
 		notSoCleverbot = new Chatbot("Mr. Clever");
 		startMessage = "Welcome to the " + notSoCleverbot.getName() + " Chatbot, type in your name.";
 	}
@@ -34,6 +46,7 @@ public class ChatbotAppController
 	 * Allows other object access to the notSoCleverbot.
 	 * @return The Chatbot for this app.
 	 */
+	
 	public Chatbot getNotSoCleverbot()
 	{
 		return notSoCleverbot;
@@ -41,24 +54,28 @@ public class ChatbotAppController
 	/**
 	 * Starts the Chatbot Application
 	 */
+	
 	public void start()
 	{
 		String message = appView.displayChatbotConversations(startMessage);
-		while(!notSoCleverbot.quitChecker(message))
-		{
-			message = notSoCleverbot.processText(message);
-			if(message.equals("I've said too much!"))
-			{
-				message = appView.displayChatbotConversations(message);
-				quit();
-			}
-			message = appView.displayChatbotConversations(message);
-		}
-		quit();
+//
+//		 while(!notSoCleverbot.quitChecker(message))
+//		 
+//		{
+//			message = notSoCleverbot.processText(message);
+//			if(message.equals("I've said too much!"))
+//			{
+//				message = appView.displayChatbotConversations(message);
+//				quit();
+//			}
+//			message = appView.displayChatbotConversations(message);
+//		}
+//		quit();
 	}
 	/**
 	 * Ends and closes the program completely
 	 */
+	
 	private void quit()
 	{
 		appView.displayInformation("Good bye cruel user :( I hope to never see you again.");
